52275095fe0d5d9ef35eb773e378178722fbcd72
zaseevav <zaseevav@yandex.ru>
2021-05-11 15:33:18 +0300

    add Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import "animate.css/animate.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
