70cf60a014f2ad0341d13a0b294196d4c89dad78
zaseevav <zaseevav@yandex.ru>
2021-05-12 10:11:32 +0300

    remove Slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import EventsList from '../components/EventsList';
 const Home: React.FC = () => {
   return (
     <>
-      <div className="">Slider</div>
       <div className="">
         <EventsList />
       </div>
